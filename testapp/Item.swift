c6a00ed40965ead01e9f4dff96aecf79bd5549ac
Hassler420 <hmaximilian.hassler@gmail.com>
2025-09-29 23:21:29 +0200

    Initial Commit

---
+++ testapp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  testapp
+//
+//  Created by Maximilian Hassler on 29.09.25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
